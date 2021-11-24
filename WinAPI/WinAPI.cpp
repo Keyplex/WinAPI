742990058488e7490fa71a865b62dbe2ccad252c
Pavel <51826654+1commando1@users.noreply.github.com>
2021-11-24 20:08:08 +0300

    Добавьте файлы проекта.

---
+++ WinAPI/WinAPI.cpp
@@ -0,0 +1,25 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	system("chcp 1251>nul");
+	int message = MessageBox(NULL, L"Привет", L"Мир", MB_YESNOCANCEL);
+	switch (message) {
+	case IDYES:{
+		MessageBox(NULL, L"Вы нажали кнопку Да", L"Мир", NULL);
+			break;
+	}
+	case IDNO: {
+		MessageBox(NULL, L"Вы нажали кнопку Нет", L"Мир", NULL);
+			break;
+	}
+	case IDCANCEL: {
+		MessageBox(NULL, L"Вы нажали кнопку Отмена", L"Мир", NULL);
+		break;
+	}
+	}
+	return 0;
+}
